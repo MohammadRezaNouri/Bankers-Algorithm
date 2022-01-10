f32e94b076402b103624b2055a77e58c998bcca7
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-01-11 00:15:17 +0330

    Get number of processe and resource

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main()
 {
+    int p, r;
+    cout << "Enter number of processes : ";
+    cin >> p;
+    cout << "Enter number of resources : ";
+    cin >> r;
     return 0;
 }
